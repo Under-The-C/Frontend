c36da9a90f8c5129d5c5da60f5af7ff4fc779945
OhHyukJun <kmh7277@naver.com>
2023-08-03 17:01:47 +0900

    Feat: 항목별 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Main } from "./Pages/Main/Main";
-
-
+import { Customer } from "./Pages/Customer/Customer";
+import { Category } from "./Pages/Categories/Category";
 const App = () => {
   return (
     <>
@@ -11,6 +11,8 @@ const App = () => {
       <BrowserRouter>  
         <Routes>
           <Route path="/" element={<Main/>}/>
+          <Route path="/Category/:itemName" element={<Category/>}/>
+          <Route path="/Customer" element={<Customer/>}/>
         </Routes>
       </BrowserRouter>
     </RecoilRoot>
