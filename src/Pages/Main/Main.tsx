87ddb3410b4f8646d5d53c29a253b5bf43afd8a6
OhHyukJun <kmh7277@naver.com>
2023-08-05 20:35:21 +0900

    Style: 패딩 마진

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -7,70 +7,63 @@ import cereal from "../../img/Cereal.jpg";
 import { Link } from "react-router-dom";
 import { useQuery, useQueryClient } from "react-query";
 
+const MainContainer = styled(Container)`
+  padding-bottom: 10vh;
+`;
 
 const CenteredContainer = styled(Container)`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 5vh;
+  height: 40vh;
 `;
-const HorizontalRow = styled(Row)`
-  display: flex;
-  justify-content: space-around;
-  gap: 10vw;
-`;
+
 const TopComponent = styled.img`
   width: 100%;
   height: auto;
-  max-height: calc(60vh);
+  max-height: calc(40vh);
   object-fit: contain;
   margin-top: 2vh;
+  margin-bottom: 5vh;
 `;
+
 const SmallComponent = styled.img`
   width: 100%;
   height: auto;
-  max-height: calc(30vh);
+  max-width: 100%;
   object-fit: contain;
-  margin-bottom: 5vh;
 `;
+
 export const Main = () => {
-  /*
-	const { isLoading, isError, data } = useQuery([],  {
-			 
-			//기본 캐시 타임 == 5min
-		});
-		if (isLoading)
-		return <h2>Loading...</h2>
-		
-		if (isError)
-			return <h2>Error...</h2>
-*/
+  // ...
   return (
     <>
-      <TopComponent src={test} alt="react" />
+    <MainContainer>
+      <Row className="p-5">
+        <TopComponent src={fruit} alt="react" />
+      </Row>
       <CenteredContainer>
-        <div>
-          <Container className="p-5">
-            <HorizontalRow className="p-5">
-              <div>
+        <Container className="p-5">
+          <Row className="p-5">
+            <Col>
               <Link to="/Category/Fruit">
-              <SmallComponent src={fruit} alt="react"/>
-            </Link>
-              </div>
-              <div>
+                <SmallComponent src={fruit} alt="react" />
+              </Link>
+            </Col>
+            <Col>
               <Link to="/Category/Vegetable">
-              <SmallComponent src={vegetable} alt="react"/>
-            </Link>
-              </div>
-              <div>
+                <SmallComponent src={vegetable} alt="react" />
+              </Link>
+            </Col>
+            <Col>
               <Link to="/Category/Cereal">
-              <SmallComponent src={cereal} alt="react"/>
-            </Link>
-              </div>
-            </HorizontalRow>
-          </Container>
-        </div>
+                <SmallComponent src={cereal} alt="react" />
+              </Link>
+            </Col>
+          </Row>
+        </Container>
       </CenteredContainer>
+      </MainContainer>
     </>
   );
 };
