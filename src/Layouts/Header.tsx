0e60ee2b9fc989f5e669d7ce90e265f000167308
Osongmin <autumninmoon@gmail.com>
2023-08-06 01:15:16 +0900

    fix: href -> to

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -60,9 +60,13 @@ const Header = () => {
           </Form>
           <Nav className="ms-auto">
             <NavDropdown title="카테고리" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/Category/과일">과일</NavDropdown.Item>
-              <NavDropdown.Item href="/Category/채소">채소</NavDropdown.Item>
-              <NavDropdown.Item href="/Category/견과">
+              <NavDropdown.Item as={Link} to="/Category/과일">
+                과일
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/Category/채소">
+                채소
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/Category/견과">
                 쌀, 잡곡, 견과
               </NavDropdown.Item>
             </NavDropdown>
