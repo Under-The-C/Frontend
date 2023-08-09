8da7ac156116243ba66d4705e480d5d19d298578
OhHyukJun <kmh7277@naver.com>
2023-08-09 17:39:44 +0900

    Feat: selector 추가

--- src/Atom/buy.ts
+++ src/Atom/buy.ts
@@ -1,6 +1,7 @@
 import { atom, selector } from "recoil";
 import { BuyItem } from "../interface/buy";
-
+import { Buyer } from "../interface/user";
+import { userState } from "./user";
 export const buyState = atom<BuyItem>({
   key: "buyState",
   default: {
@@ -21,4 +22,34 @@ export const buyState = atom<BuyItem>({
 export const formDataState = atom<FormData | null>({
   key: "formDataState",
   default: null,
+});
+
+export const buyerState = atom<Buyer>({
+  key: "buyerState",
+  default:{
+    buyerId: 0,
+    buyerName: "",
+    buyerEmail: "",
+    buyerAddress: "",
+    buyerPhone: "",
+  },
+});
+
+export const buyeruserState = selector({
+  key: "sellerState",
+  get: ({ get }) => {
+    const user = get(userState);
+    const buyerValue = get(buyerState);
+
+    return {
+      user,
+      buyerValue,
+    };
+  },
+  set: ({ set }, newValue: any) => {
+    
+    if (newValue.user) set(userState, newValue.user);
+    if (newValue.buyerValue) set(buyerState, newValue.buyerValue);
+    
+   },
 });
