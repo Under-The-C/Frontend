3a1698c8225a78285aad642abda721c679c68424
OhHyukJun <kmh7277@naver.com>
2023-08-13 14:26:12 +0900

    Feat: 결제 api 추가

--- src/Pages/Payment/BuyerForm.tsx
+++ src/Pages/Payment/BuyerForm.tsx
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import styled from "styled-components";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Button as BootstrapButton,
+  Container,
+  Col,
+  Row,
+  FormControl,
+} from "react-bootstrap";
 import { buyerState } from "../../Atom/buy";
 import { Buyer } from "../../interface/user";
+import { SalesDto } from "../../interface/sales";
 import { buyeruserState } from "../../Atom/buy";
+import { salesState } from "../../Atom/sales";
+import {SERVER} from "../../config";
+import axios from "axios";
 
 const buyerPerson: Buyer = {
   buyerId: 1,
@@ -17,6 +27,7 @@ const buyerPerson: Buyer = {
 const Text = styled.span`
     width:5vw;
 `;
+
 
 
 const FormContainer = styled(Container)`
@@ -37,11 +48,33 @@ const StyledCol = styled(Col)`
   margin-bottom: 2vw;
 `;
 
+const Button = styled(BootstrapButton)`
+  width: 30%;
+  margin-top:3vw;
+  display: flex;
+  height: auto;
+  justify-content: center;
+  item-align: left;
+`;
 
 
 export const BuyerForm = () => {
   const [buyerUser, setBuyerUser] = useRecoilState(buyeruserState);
+  const product = useRecoilValue(salesState);
   const [buyer, setBuyer] = useRecoilState(buyerState);
+  const [pay, setPay] = useState("");
+
+  const onClick = async () => {
+    try {
+      const response = await axios.post("https://115.85.181.92/api/v1/payment/web-hook");
+      console.log(response);
+      setPay(response.data);
+    } catch (error: any) {
+      console.log(error);
+      alert(`에러: ${error.message}`);
+    }
+  };
+  
   useEffect(() => {
     setBuyerUser({
       ...buyerUser,
@@ -103,6 +136,19 @@ export const BuyerForm = () => {
           onChange={inputChange}
           className="w-[55vw] h-[3rem] pl-2 border-none outline-none rounded-lg bg-slate-100"
         />
+      </StyledCol>
+     
+      <StyledCol>
+      <Text>결제정보</Text>
+      </StyledCol>
+      <StyledCol>
+      <Text>결제 금액</Text>
+        <span className="w-[55vw] flex justify-end">
+          {product.price}원
+        </span>
+      </StyledCol>
+      <StyledCol>
+        <Button onClick={onClick} className="bg-mainGreen">결제하기</Button>
       </StyledCol>
     </FormContainer>
     </>
