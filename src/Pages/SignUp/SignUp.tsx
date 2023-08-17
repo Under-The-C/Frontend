b01ddd2b30c610711e2125218a45e5b4a0324e7c
Osongmin <autumninmoon@gmail.com>
2023-08-18 00:55:05 +0900

    fix: 헤더에 멀티파티폼 명시

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -94,11 +94,11 @@ export const SignUp = () => {
     const res = await axios.post(
       SERVER.SERVER_API +
         `/v1/user/add?access_token=${searchParams.get("access_token")}`,
+      signup,
       {
-        Headers: {
+        headers: {
           "Content-Type": "multipart/form-data",
         },
-        signup,
       }
     );
     console.log(res.data);
