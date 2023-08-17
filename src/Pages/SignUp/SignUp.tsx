48c800038c659edda058d7b8c6901c0de8302004
OhHyukJun <kmh7277@naver.com>
2023-08-17 21:43:48 +0900

    Feat: 회원가입 수정

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -91,18 +91,18 @@ export const SignUp = () => {
     console.log(signup);
     const formData = new FormData();
     if (signup.certificate) {
-      formData.append('certificate', signup.certificate);
-    }
-  
-    const blob = new Blob([JSON.stringify(signup)], {
-      type: 'application/json',
-    });
-    const config = {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    };
-    formData.append('info', blob);
+    formData.append('certificate', signup.certificate);
+  }
+
+  const blob = new Blob([JSON.stringify(signup)], {
+    type: 'application/json',
+  });
+  const config = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  };
+  formData.append('info', blob);
     const res = await axios.post(
       SERVER.SERVER_API +
         `/v1/user/add?access_token=${searchParams.get("access_token")}`,
