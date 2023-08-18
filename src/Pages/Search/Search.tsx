406dd06363feccf541f4066ba63bb552a2b09158
Osongmin <autumninmoon@gmail.com>
2023-08-18 22:20:24 +0900

    refactor: search

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -3,96 +3,29 @@ import { useParams } from "react-router-dom";
 import { productDto } from "../../interface/product";
 import { Container } from "react-bootstrap";
 import { SearchItemSummay } from "./SearchItemSummay";
-
-const dummydata: productDto[] = [
-  {
-    id: 1,
-    seller_id: 1,
-    name: "테스트",
-    subTitle: "테스트",
-    price: 1000,
-    description: "테스트",
-    subDescription: "테스트",
-    main_image: "https://i.imgur.com/1QZzX3j.png",
-    detailImage: ["https://i.imgur.com/1QZzX3j.png"],
-    keyword: ["test"],
-    saleStartDate: "2021-09-01",
-    saleEndDate: "2023-09-01",
-    category: "채소",
-    viewCount: 12,
-    createdAt: "2021-09-01",
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    seller_id: 1,
-    name: "테스트",
-    subTitle: "테스트",
-    price: 1000,
-    description: "테스트",
-    subDescription: "테스트",
-    main_image: "https://i.imgur.com/1QZzX3j.png",
-    detailImage: ["https://i.imgur.com/1QZzX3j.png"],
-    keyword: ["test"],
-    saleStartDate: "2021-09-01",
-    saleEndDate: "2023-09-01",
-    category: "채소",
-    viewCount: 12,
-    createdAt: "2021-09-01",
-    rating: 4.5,
-  },
-  {
-    id: 3,
-    seller_id: 1,
-    name: "테스트",
-    subTitle: "테스트",
-    price: 1000,
-    description: "테스트",
-    subDescription: "테스트",
-    main_image: "https://i.imgur.com/1QZzX3j.png",
-    detailImage: ["https://i.imgur.com/1QZzX3j.png"],
-    keyword: ["test"],
-    saleStartDate: "2021-09-01",
-    saleEndDate: "2023-09-01",
-    category: "채소",
-    viewCount: 12,
-    createdAt: "2021-09-01",
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    seller_id: 1,
-    name: "테스트",
-    subTitle: "테스트",
-    price: 1000,
-    description: "테스트",
-    subDescription: "테스트",
-    main_image: "https://i.imgur.com/1QZzX3j.png",
-    detailImage: ["https://i.imgur.com/1QZzX3j.png"],
-    keyword: ["test"],
-    saleStartDate: "2021-09-01",
-    saleEndDate: "2023-09-01",
-    category: "채소",
-    viewCount: 12,
-    createdAt: "2021-09-01",
-    rating: 4.5,
-  },
-];
+import axiosInstance from "../../API/axios";
 
 export const Search = () => {
   const search = useParams().word;
-  const [products, setProducts] = useState<productDto[]>(dummydata);
+  const [products, setProducts] = useState<productDto[]>();
   const order = ["최신순", "조회순", "리뷰순", "저가순"];
+  const [orderClick, setOrderClick] = useState("최신순");
+
+  const fetchSearchData = async () => {
+    const res = await axiosInstance.get(
+      `/v1/search_product/all?Name=${search}&sortBy=${orderClick}`
+    );
+    setProducts(res.data);
+  };
 
   useEffect(() => {
-    //fetch
+    fetchSearchData();
   }, []);
 
   const handleOrder = (e: any) => {
     const id = e.target.id;
-    console.log(id);
-
-    // setProduct (fetch(`http://localhost:8080/product/search/${search}/${id}`
+    setOrderClick(id);
+    fetchSearchData();
   };
 
   return (
@@ -114,9 +47,10 @@ export const Search = () => {
         </div>
       </div>
       <div className="mt-5 grid grid-cols-3 gap-4 w-full h-full">
-        {products.map((item: productDto) => (
-          <SearchItemSummay key={item.id} props={item} />
-        ))}
+        {products &&
+          products.map((item: productDto) => (
+            <SearchItemSummay key={item.id} props={item} />
+          ))}
       </div>
     </Container>
   );
